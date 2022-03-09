bafdcaf395101c4bc7201645ae7b7bb73434146e
Salih <salihyakuphankeskin@gmail.com>
2022-03-09 12:16:09 +0300

    Update lesson4.cpp

--- lesson4.cpp
+++ lesson4.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+void example( const int x, const int y, int& sum, int& product)
+{
+    sum= x+y;
+    product =x*y;
+
+}
+
+int main()
+{
+    int s,p,c =10;
+    double r,t = 3.2;
+    example(c,t,s,p);
+    cout<< s<< endl;
+    cout<< p<< endl;
+    cout<< c<< endl;
+    cout<< t<< endl;
+    return 0;
+}
+/*
+
+*/
